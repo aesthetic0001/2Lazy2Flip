e8d84e53f8356573ad0b870ac197d307d7b4e2a6
aesthetic <15858616+aesthetic0001@users.noreply.github.com>
2021-11-17 18:21:07 -0500

    add sales in embed

--- nec.js
+++ nec.js
@@ -60,6 +60,7 @@ async function initialize() {
                             .addField("Auction", `/viewauction ${result.auctionData.auctionID}`, true)
                             .addField("Item LBIN", `${currencyFormat.format(result.auctionData.lbin)}`, true)
                             .addField("Expected profit", `${currencyFormat.format(result.auctionData.profit)}`, true)
+                            .addField("Sales/Day", `${currencyFormat.format(result.auctionData.profit)}`, true)
                             .setTimestamp()]
                     });
                 }
