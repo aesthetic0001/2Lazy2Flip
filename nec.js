475c8f263333242898c48b61c9c2d5d00d612fb8
DrMoraschi <68395930+amoraschi@users.noreply.github.com>
2021-11-19 18:00:07 +0100

    resolve conflict

--- nec.js
+++ nec.js
@@ -55,6 +55,7 @@ async function initialize() {
         workers[j].on("message", async (result) => {
             if (result.itemData !== undefined) {
                 servUtils.newFlip(result)
+                console.log(result, "WORKER " + j)
                 if (config.webhook.useWebhook) {
                     await webhook.send({
                         username: config.webhook.webhookName,
