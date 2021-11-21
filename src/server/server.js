6abf6410a308656ffaffe06ac35b195d72bd9dec
DrMoraschi <68395930+amoraschi@users.noreply.github.com>
2021-11-21 12:26:56 +0100

    solve again

--- src/server/server.js
+++ src/server/server.js
@@ -15,7 +15,7 @@ async function initServer () {
 		})
 
 		serv.listen(port, () => {
-			console.log(`Webpage started on localhost:${port}`)
+			console.log(`Webpage started on http://localhost:${port}/`)
 		})
 }
 
