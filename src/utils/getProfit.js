73c85df5f0c2f60e83ce05be700a839af33733c6
aesthetic <15858616+aesthetic0001@users.noreply.github.com>
2021-11-23 22:02:48 -0500

    round the #s

--- src/utils/getProfit.js
+++ src/utils/getProfit.js
@@ -3,15 +3,15 @@ function getProfit(price, rcCost, lbin) {
     if (price >= 1000000) {
         profitItem.RCProfit = ((lbin + rcCost) - price)
             - ((lbin + rcCost) * 0.02);
-        profitItem.RCPP = (profitItem.RCProfit * 100) / lbin
+        profitItem.RCPP = parseFloat(((profitItem.RCProfit * 100) / lbin).toFixed(1))
         profitItem.snipeProfit = (lbin - price) - (lbin * 0.02)
-        profitItem.snipePP = (profitItem.snipeProfit * 100) / lbin
+        profitItem.snipePP = parseFloat(((profitItem.snipeProfit * 100) / lbin).toFixed(1))
     } else {
         profitItem.RCProfit = ((lbin + rcCost) - price)
             - ((lbin + rcCost) * 0.01);
-        profitItem.RCPP = (profitItem.RCProfit * 100) / lbin
+        profitItem.RCPP = parseFloat(((profitItem.RCProfit * 100) / lbin).toFixed(1))
         profitItem.snipeProfit = (lbin - price) - (lbin * 0.01)
-        profitItem.snipePP = (profitItem.snipeProfit * 100) / lbin
+        profitItem.snipePP = parseFloat(((profitItem.snipeProfit * 100) / lbin).toFixed(1))
     }
 
     return profitItem
